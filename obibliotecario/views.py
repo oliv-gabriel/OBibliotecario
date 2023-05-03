5b4ccdb2e54971284422597640a9e5c49e448179
oliv-gabriel <gabrielgamebr102@gmail.com>
2023-05-03 00:16:26 -0400

    html e views

--- obibliotecario/views.py
+++ obibliotecario/views.py
@@ -1,15 +1,18 @@
-from django.shortcuts import render
-import json
+
+from django.shortcuts import render, redirect
 import requests
 
-
 def index(request):
+    
+    return render(request, 'obibliotecario/index.html' )
+
+
+def pesquisa(request):
+    genero = request.POST.get('genero')
+    autor = request.POST.get('autor')
+    ano = request.POST.get('ano')
 
     API_KEY = 'AIzaSyDGOeC_b875-tGdokQz09EEutLD1TEy6sY'
-
-    genero = input('Digite o gênero do livro que deseja buscar: ')
-    autor = input('Digite o nome do autor que deseja buscar: ')
-    ano = input('Digite o ano de publicação que deseja buscar: ')
 
     parametros = {
         'q': '',
@@ -31,23 +34,28 @@ def index(request):
     url = 'https://www.googleapis.com/books/v1/volumes'
     response = requests.get(url, params=parametros)
 
-    # AQUI
-    dados = response.json()
-    dados = 'dados'
-
-    if 'items' in dados:
-        for item in dados['items']:
-            print(item)
-            titulo= item['volumeInfo'].get('title', 'Título desconhecido')
-            autores= item['volumeInfo'].get('authors', ['Autor desconhecido'])
-            print(f'- {titulo}, por {", ".join(autores)}')
-    else:
-        print('Nenhum resultado encontrado.')
-
-    return render(request, 'obibliotecario/index.html', )
+    livros = response.json()
+    return livros
 
 
-def livros(request,):
-    dados = 'dadosads'
-    
-    return render(request, 'obibliotecario/livros.html', {'dados': dados})
+
+
+def livros(request):
+    livros_api = pesquisa(request)
+    livros = []
+    for item in livros_api.get('items', []):
+        volume_info = item.get('volumeInfo', {})
+        livro = {
+            'titulo': volume_info.get('title', ''),
+            'autor': ', '.join(volume_info.get('authors', [])),
+            'descricao': volume_info.get('description', ''),
+        }
+        imagem_links = volume_info.get('imageLinks', {})
+        if 'thumbnail' in imagem_links:
+            livro['imagem'] = imagem_links['thumbnail']
+        else:
+            livro['imagem'] = None
+        livros.append(livro)
+
+    return render(request, 'obibliotecario/livros.html', {'livros': livros})
+
